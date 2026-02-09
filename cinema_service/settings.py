3e02db76276e3a52e6f88f470256f297b5796deb
nastya <hociy2015@gmail.com>
2026-02-09 14:41:57 +0200

    Solution1

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -143,10 +143,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
